bf6a415dccdb6d953f11e353cf3be7a119eebd8b
southorange <15280970040@163.com>
2022-04-06 12:08:27 +0800

    fix: ts type error

--- src/button/index.svelte
+++ src/button/index.svelte
@@ -1,18 +1,39 @@
-<button on:click class:disabled={disabled} class={className}>
-    <slot />
-</button>
-
 <script lang="ts">
-    export {
-        className as class,
-        disabled
-    }
-    let className: string = undefined;
-    let disabled: boolean = undefined;
+  import classNames from "classnames";
+  export { className as class, disabled };
+  let className: string = undefined;
+  let disabled: boolean = undefined;
 </script>
 
+<button
+  on:click
+  class={classNames("o-button", className, {
+    "o-is-disabled": disabled,
+  })}
+>
+  <slot />
+</button>
+
 <style>
-.disabled {
-    cursor: not-allowed;
-}
+  .o-button {
+    font-size: 14px;
+    height: 32px;
+    padding: 0 14px;
+    background-color: white;
+    border: 1px solid rgb(220, 220, 220);
+    border-radius: 4px;
+  }
+  .o-button:hover {
+    animation: .5s bg 0s linear;
+  }
+  @keyframes bg {
+    0% {
+      color: black;
+      border-color: rgb(220, 220, 220);
+    }
+    100% {
+      color: rgb(38, 111, 232);
+      border-color: rgb(38, 111, 232);
+    }
+  }
 </style>
